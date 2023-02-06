64085af82c9141c71fcbdf0a23994d4038efba40
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-02-06 13:07:25 +0300

    Adding 3 components

---
+++ src/component/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Concat() {
+    return (
+        <address>
+            You can find us here:<br />
+            GeeksforGeeks<br />
+            5th & 6th Floor, Royal Kapsons, A- 118, <br />
+            Sector- 136, Noida, Uttar Pradesh (201305)
+        </address>
+    )
+}
+
+export default Concat
